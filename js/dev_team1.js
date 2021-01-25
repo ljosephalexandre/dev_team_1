ae7f6e7b67a07934d05fb1b241b4c2bb042c7f23
Martin <lefils.martin@gmail.com>
2021-01-25 16:28:13 +0100

    Revert "Revert "Essaie raté n1""
    This reverts commit dc565034e12af1fa0d44697a8b569be151ce87a3.

--- js/dev_team1.js
+++ js/dev_team1.js
@@ -2,3 +2,9 @@ $(function () {
     $("#sortable").sortable();
     $("#sortable").disableSelection();
 });
+
+var addList = document.getElementsByClassName("addList");
+let newLi = document.createElement('li');
+
+
+addList.onclick = function() { newLi.textContent = 'li'; };
